beda67c480d824915fefd8a166607b7cff3c0356
Carlosher007 <carlos.hernandez.agudelo@correounivalle.edu.co>
2023-07-10 17:54:22 -0500

    FInal dia 2

---
+++ Curso/Dia_2/dia2.py
@@ -0,0 +1,25 @@
+# VARIABLES
+pais = "Mexico"
+
+nombre = input("Escribe tu nombre: ")
+
+# CONVERSIONES
+
+unoT = "1"
+unoI = int(unoT)
+unoF = float(unoT)
+
+# FORMATEO
+
+color_auto = "rojo"
+matricula = "kwsdkwd"
+print ("Mi auto es {} y de matricula {}".format(color_auto,matricula))
+print(f"Mi auto es de {color_auto} y de matricula {matricula}")
+
+# REDONDEO
+
+number = 8.989
+ndigits = 1
+
+round(number,ndigits)
+
--- Proyectos/MisProyectos/manageOptions.py
+++ Proyectos/MisProyectos/manageOptions.py
@@ -2,12 +2,10 @@
 The idea is do a while bucle where you can ask to user if he/she wants to follow or cancel the process. This function must be able to be reusable .
 """
 
-import os
-
 def manageOptions(steps):
+
   def handleOption(option, action):
     if option.lower() == "s":
-      os.system('clear')
       action()
     elif option.lower() == "n":
       return False
@@ -16,13 +14,12 @@ def manageOptions(steps):
     return True
 
   def bucle():
-    os.system('clear')
     steps()
 
     while True:
-      option = input("\nDo you want to continue? (S/N) ")
+      option = input("Do you want to continue? (S/N) ")
       if not handleOption(option, steps):
         break
 
   bucle()
-  
+
